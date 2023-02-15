8b44f3d4203f4196f476097fb2906947f35cd828
farid <faridhimeur@outlook.fr>
2023-02-15 17:07:21 +0100

    Create route Challenges itsok

--- backend/database.sql
+++ backend/database.sql
@@ -5,30 +5,24 @@ CREATE TABLE item (
 
 INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
 
-CREATE TABLE category
-(
-	id            INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	category_name VARCHAR(100)        NOT NULL
-) engine = InnoDB
-  default charset = latin1;
+CREATE TABLE category (
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  category_name VARCHAR(100) NOT NULL
+) engine = InnoDB default charset = latin1;
 
-INSERT INTO category (category_name)
-VALUES ('Restaurant'),
-	   ('Evenement');
+INSERT INTO category (category_name) VALUES ('Restaurant'), ('Evenement');
 
-CREATE TABLE challenge
-(
-	id                INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	title             VARCHAR(200)        NOT NULL,
-	description_challenge VARCHAR(900)        NOT NULL,
-	points              NUMBER(100)        NOT NULL,
-	tips VARCHAR(200) NOT NULL,
-	category_id       INT                 NOT NULL,
+CREATE TABLE challenge (
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title VARCHAR(200) NOT NULL,
+  description_challenge VARCHAR(900) NOT NULL,
+  points VARCHAR(100) NOT NULL,
+  tips VARCHAR(200) NOT NULL,
+	category_id INT NOT NULL,
 	CONSTRAINT fk_challenge_category
 		FOREIGN KEY (category_id)
 			REFERENCES category (id)
-) engine = InnoDB
-  default charset = utf8mb4;
+) engine = InnoDB default charset = utf8mb4;
 
 INSERT INTO challenge (title, description_challenge, points, tips, category_id)
-VALUES ('Lorem', 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..', 2, 'Lorem ipsum dolor sit amet.', 1);
+VALUES ('Lorem', 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..', "2", 'Lorem ipsum dolor sit amet.', 1);
