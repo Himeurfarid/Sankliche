fa7f3301feec14015410280e6b2d80532f4bccaf
Lucie <99013579+LuciieF@users.noreply.github.com>
2023-02-15 15:57:15 +0100

    challenge table

--- backend/database.sql
+++ backend/database.sql
@@ -10,19 +10,14 @@ CREATE TABLE challenge
 	id                INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	title             VARCHAR(200)        NOT NULL,
 	description_challenge VARCHAR(900)        NOT NULL,
-	publication_date  date                NOT NULL,
-	link              VARCHAR(100)        NOT NULL,
-	thumbnail VARCHAR(200) NOT NULL,
+	points              NUMBER(100)        NOT NULL,
+	tips VARCHAR(200) NOT NULL,
 	category_id       INT                 NOT NULL,
-	CONSTRAINT fk_video_category
+	CONSTRAINT fk_challenge_category
 		FOREIGN KEY (category_id)
 			REFERENCES category (id)
 ) engine = InnoDB
   default charset = utf8mb4;
 
-INSERT INTO video (title, description_video, publication_date, link, thumbnail, category_id)
-VALUES ('NBA - Résumé VF : Golden State Warriors @ Los Angeles Lakers',
-		'Avec un Stephen Curry en triple-double, son premier depuis 2016 et un Jordan Poole inspiré, les Warriors ont renversé les Lakers malgré les 67 pts du duo James-Davis.',
-		'2021-10-20', 'https://www.youtube.com/embed/tSGtWkQ30no',
-		'https://i.ytimg.com/vi/fmuDBDbYwYE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB1LN1SOPKkLmxK_fXR__wI-ljluQ',
-		2);
+INSERT INTO challenge (title, description_challenge, points, tips, category_id)
+VALUES ('Lorem', 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..', 2, 'Lorem ipsum dolor sit amet.', 1);
