e4fa88e95d6222e4525a1650ff4fefdf827c1292
farid <faridhimeur@outlook.fr>
2023-02-16 15:43:22 +0100

    csschallenge

--- frontend/src/components/ChallengesCard.jsx
+++ frontend/src/components/ChallengesCard.jsx
@@ -3,33 +3,7 @@ import "./ChallengesCard.css";
 function Challenge() {
   return (
     <div className="challenge">
-      <div className="challengescardeasy">
-        <div className="titleandlevelchallenges">
-          <h1 className="titlechallenge">Titre du défi</h1>
-          <p className="levelchallenge">Difficulté du défi</p>
-        </div>
-        <div className="descriptionchallenges">
-          <p className="descriptionchallenge">description du défi</p>
-        </div>
-        <div className="timeandpointchallenges">
-          <p className="timechallenge">Temps du défi</p>
-          <p className="scorechallenge">Point du défi</p>
-        </div>
-      </div>
-      <div className="challengescardnormal">
-        <div className="titleandlevelchallenges">
-          <h1 className="titlechallenge">Titre du défi</h1>
-          <p className="levelchallenge">Difficulté du défi</p>
-        </div>
-        <div className="descriptionchallenges">
-          <p className="descriptionchallenge">description du défi</p>
-        </div>
-        <div className="timeandpointchallenges">
-          <p className="timechallenge">Temps du défi</p>
-          <p className="scorechallenge">Point du défi</p>
-        </div>
-      </div>
-      <div className="challengescardhard">
+      <div className="challengescard">
         <div className="titleandlevelchallenges">
           <h1 className="titlechallenge">Titre du défi</h1>
           <p className="levelchallenge">Difficulté du défi</p>
