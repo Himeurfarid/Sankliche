13f8eac42a20b6d378c72b0d80afd0a100077a55
farid <faridhimeur@outlook.fr>
2023-02-16 13:01:08 +0100

    make a minimal card css and html of a challenge with display grid

---
+++ frontend/src/components/ChallengesCard.jsx
@@ -0,0 +1,21 @@
+import "./ChallengesCard.css";
+
+function Challenge() {
+  return (
+    <div className="challengescard">
+      <div className="titleandlevelchallenges">
+        <h1 className="titlechallenge">Titre du défi</h1>
+        <p className="levelchallenge">Difficulté du défi</p>
+      </div>
+      <div className="descriptionchallenges">
+        <p className="descriptionchallenge">description du défi</p>
+      </div>
+      <div className="timeandpointchallenges">
+        <p className="timechallenge">Temps du défi</p>
+        <p className="scorechallenge">Point du défi</p>
+      </div>
+    </div>
+  );
+}
+
+export default Challenge;
