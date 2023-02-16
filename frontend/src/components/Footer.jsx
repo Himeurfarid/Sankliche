a674465db2be50f53fe3050f77f719f117fcea5d
AlexisCode22 <durinalexis@gmail.com>
2023-02-16 12:00:03 +0100

    create buttons footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,26 @@
+import home from "../assets/home.png";
+import cible from "../assets/cible.png";
+import profil from "../assets/profil.png";
+import tips from "../assets/tips.png";
+import "../styles/Footer.css";
+
 function Footer() {
-  return <p>Footer</p>;
+  return (
+    <div className="footer">
+      <button type="button">
+        <img src={home} alt="home" className="home" />
+      </button>
+      <button type="button">
+        <img src={cible} alt="cible" className="cible" />
+      </button>
+      <button type="button">
+        <img src={profil} alt="profil" className="profil" />
+      </button>
+      <button type="button">
+        <img src={tips} alt="tips" className="tips" />
+      </button>
+    </div>
+  );
 }
 
 export default Footer;
