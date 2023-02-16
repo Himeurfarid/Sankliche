7aec5035ec78c082986ff967604555e5b6bca5a9
farid <faridhimeur@outlook.fr>
2023-02-16 11:27:42 +0100

    add folder components and components Challenges

---
+++ frontend/src/components/Challenges.jsx
@@ -0,0 +1,11 @@
+import Challenges from "./Challenges";
+
+function Challenge() {
+  return (
+    <div className="challenges">
+      <p> Challenges </p>
+    </div>
+  );
+}
+
+export default Challenge;
