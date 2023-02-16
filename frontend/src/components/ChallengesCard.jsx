f76fa2bf1f5fda104399b6ffa673051edf80f51a
farid <faridhimeur@outlook.fr>
2023-02-16 16:44:17 +0100

    css

--- frontend/src/components/ChallengesCard.jsx
+++ frontend/src/components/ChallengesCard.jsx
@@ -1,19 +1,18 @@
 import "./ChallengesCard.css";
+import time from "../assets/time.png";
 
 function Challenge() {
   return (
-    <div className="challenge">
-      <div className="challengescard">
-        <div className="titleandlevelchallenges">
-          <h1 className="titlechallenge">Titre du défi</h1>
-          <p className="levelchallenge">Difficulté du défi</p>
-        </div>
-        <div className="descriptionchallenges">
-          <p className="descriptionchallenge">description du défi</p>
-        </div>
-        <div className="timeandpointchallenges">
-          <p className="timechallenge">Temps du défi</p>
-          <p className="scorechallenge">Point du défi</p>
+    <div className="challengescard">
+      <div className="titleandlevelchallenges">
+        <h1 className="titlechallenge">Miam-Miam !</h1>
+        <p className="levelchallenge">A1 - Facile</p>
+      </div>
+      <div className="timeandpointchallenges">
+        <p className="scorechallenge"> +5 pts</p>
+        <div className="logoandtime">
+          <img className="logotime" src={time} alt="time" />
+          <p className="timechallenge">10 min</p>
         </div>
       </div>
     </div>
