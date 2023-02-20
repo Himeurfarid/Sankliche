8de8ac71e0fcbd4f554b00c4391549669613c41f
farid <faridhimeur@outlook.fr>
2023-02-20 18:28:48 +0100

    challenge by id

--- frontend/src/components/ChallengesDetails.jsx
+++ frontend/src/components/ChallengesDetails.jsx
@@ -1,9 +1,11 @@
-function ChallengeDetails() {
+function ChallengesDetails() {
   return (
     <div>
-      <div className="titleandlevelchallenges">OKOK</div>
+      <div className="titleandlevelchallenges">
+        <h1 className="titlechallenge">Titre</h1>
+      </div>
     </div>
   );
 }
 
-export default ChallengeDetails;
+export default ChallengesDetails;
