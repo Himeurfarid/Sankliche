01ab73c3e15c6af6f1be1ca411c3c838d5ec31b9
farid <faridhimeur@outlook.fr>
2023-02-21 23:51:31 +0100

    add page team and link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,11 +6,13 @@ import "../styles/Header.css";
 function Header() {
   return (
     <div className="header-app">
-      <img
-        className="logosankliche"
-        src="https://static.wixstatic.com/media/8f8b51_a6745be895cc420f83ca3d402922069f~mv2.png/v1/fill/w_94,h_94,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/logosk.png"
-        alt="logosanklché"
-      />
+      <Link to="/">
+        <img
+          className="logosankliche"
+          src="https://static.wixstatic.com/media/8f8b51_a6745be895cc420f83ca3d402922069f~mv2.png/v1/fill/w_94,h_94,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/logosk.png"
+          alt="logosanklché"
+        />
+      </Link>
       <div className="linkprofil">
         <Link to="/profil">
           <button className="buttonprofil" type="button">
--- frontend/src/pages/Tips.jsx
+++ frontend/src/pages/Tips.jsx
@@ -1,11 +1,88 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import "../styles/Tips.css";
 
 function Tips() {
   return (
     <>
       <Header />
-      <p>PAGE TIPS</p>
+      <section>
+        <h2>Astuce n°1 : Lisez en français</h2>
+        <p>
+          Lire des textes en français est un excellent moyen d'enrichir votre
+          vocabulaire et d'améliorer votre compréhension de la grammaire. Vous
+          pouvez commencer par lire des articles de journaux en ligne ou des
+          histoires courtes, et augmenter progressivement la difficulté des
+          textes.
+        </p>
+      </section>
+      <section>
+        <h2>
+          Astuce n°2 : Regardez des films et des émissions de télévision en
+          français
+        </h2>
+        <p>
+          Regarder des films et des émissions de télévision en français est un
+          excellent moyen d'améliorer votre compréhension orale. Vous pouvez
+          commencer par regarder des films avec des sous-titres en français, et
+          augmenter progressivement la difficulté en retirant les sous-titres.
+        </p>
+      </section>
+      <section>
+        <h2>Astuce n°3 : Pratiquez avec des locuteurs natifs</h2>
+        <p>
+          La pratique avec des locuteurs natifs est un excellent moyen
+          d'améliorer votre prononciation et votre fluidité. Vous pouvez trouver
+          des partenaires de conversation en ligne ou dans des groupes de
+          conversation en français dans votre région.
+        </p>
+      </section>
+      <section>
+        <h2>Contenu pédagogique : La grammaire française</h2>
+        <p>
+          La grammaire française peut être difficile pour les apprenants
+          étrangers, mais elle est essentielle pour parler et écrire
+          correctement. Voici quelques points clés de la grammaire française à
+          étudier :
+        </p>
+        <ul>
+          <li>Les genres des mots : masculin ou féminin</li>
+          <li>Les articles : définis, indéfinis et partitifs</li>
+          <li>Les verbes : conjugaison, temps et modes</li>
+          <li>Les pronoms : personnels, démonstratifs et relatifs</li>
+          <li>Les adjectifs : qualificatifs, possessifs et numéraux</li>
+        </ul>
+      </section>
+      <section className="content">
+        <h2>Contenu pédagogique : La grammaire française</h2>
+        <div className="card-deck">
+          <div className="card">
+            <h3>Genres des mots</h3>
+            <p>
+              Il est important de comprendre le genre des mots en français. Les
+              noms sont soit masculins, soit féminins, et ils ont des articles
+              et des adjectifs correspondants.
+            </p>
+          </div>
+          <div className="card">
+            <h3>Conjugaison des verbes</h3>
+            <p>
+              Les verbes en français ont des formes différentes selon le temps,
+              le mode et la personne. Il est essentiel de maîtriser la
+              conjugaison pour communiquer avec précision.
+            </p>
+          </div>
+          <div className="card">
+            <h3>Les pronoms</h3>
+            <p>
+              Les pronoms en français remplacent les noms et peuvent être sujets
+              ou objets. Les pronoms ont également des formes différentes selon
+              le genre et le nombre.
+            </p>
+          </div>
+        </div>
+      </section>
+
       <Footer />
     </>
   );
