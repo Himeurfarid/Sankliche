9aeae6c097452df6ed4e710d380717801899b273
Lucie <99013579+LuciieF@users.noreply.github.com>
2023-02-23 15:37:30 +0100

    still a basic css kms

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,17 +5,19 @@ export default function Home() {
   return (
     <main>
       <div className="section">
-        <div className="title">Sankliché</div>
-        <div className="title">
+        <div className="presentation">Sankliché</div>
+        <div className="presentation">
           Une nouvelle façon d'apprendre la langue de façon ludique où l'on peut
           apprendre seul ou en équipe. :D
         </div>
-        <button type="button" className="button">
-          Réaliser un défi
-        </button>
-        <button type="button" className="button">
-          Rejoindre une équipe
-        </button>
+        <div className="buttons">
+          <button type="button" className="button">
+            Réaliser un défi
+          </button>
+          <button type="button" className="button">
+            Rejoindre une équipe
+          </button>
+        </div>
       </div>
       <div className="section">
         <div className="title">Mes stats</div>
