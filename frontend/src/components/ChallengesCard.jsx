e7886b53ba57df6eef10f6974268d191119f10e5
farid <faridhimeur@outlook.fr>
2023-02-27 15:55:00 +0100

    tips css finished

--- frontend/src/components/ChallengesCard.jsx
+++ frontend/src/components/ChallengesCard.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import "./ChallengesCard.css";
+import "../styles/ChallengesCard.css";
 import time from "../assets/time.png";
 
 function Challenge({
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Card from "../components/Card";
-import "./Home.css";
+import "../styles/Home.css";
 
 export default function Home() {
   return (
