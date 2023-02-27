50015c5f6422a5612d9bca25084ced9f17f2fa69
Lucie <99013579+LuciieF@users.noreply.github.com>
2023-02-27 14:43:31 +0100

    usercontrollers

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,88 @@
+const models = require("../models");
+
+const getUsers = (req, res) => {
+  models.user
+    .findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+const getOneUser = (req, res) => {
+  models.user
+    .find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+const editUser = (req, res) => {
+  const user = req.body;
+
+  // TODO validations (length, format...)
+
+  user.id = parseInt(req.params.id, 10);
+
+  models.user
+    .update(user)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+const addUser = (req, res) => {
+  const user = req.body;
+
+  // TODO validations (length, format...)
+
+  models.user
+    .insert(user)
+    .then(([result]) => {
+      res.location(`/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroyUser = (req, res) => {
+  models.user
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getUsers,
+  getOneUser,
+  editUser,
+  addUser,
+  destroyUser,
+};
