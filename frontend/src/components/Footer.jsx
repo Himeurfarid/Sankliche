4b11db2ef9041ba8452e33b724c73556d1fd1864
farid <faridhimeur@outlook.fr>
2023-03-01 02:11:33 +0100

    navbar

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,81 +1,121 @@
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import "../styles/Footer.css";
 
 function Footer() {
   const [activeNavItem, setActiveNavItem] = useState("home");
+  const [activeTab, setActiveTab] = useState("home");
 
-  const handleNavItemClick = (itemId) => {
-    setActiveNavItem(itemId);
+  const handleTabClick = () => {
+    setActiveTab(activeTab);
+  };
+  const handleNavItemClick = () => {
+    setActiveNavItem(activeNavItem);
   };
 
   return (
     <div className="footer">
-      <button
-        type="button"
-        className={activeNavItem === "home" ? "button-nav" : "button-nav-not"}
-        onClick={() => handleNavItemClick("home")}
+      <NavLink
+        to="/"
+        activeclassname="activelink"
+        onClick={() => handleTabClick("home")}
       >
-        <button
-          type="button"
-          className={
-            activeNavItem === "home" ? "button-nav-not-home" : "button-nav-home"
-          }
-          label="home-icone"
-        />
-        <p>Acceuil</p>
-      </button>
-      <button
-        type="button"
-        className={activeNavItem === "target" ? "button-nav" : "button-nav-not"}
-        onClick={() => handleNavItemClick("target")}
+        <div className="button-link">
+          <button
+            type="button"
+            className={
+              activeNavItem === "home" ? "button-nav" : "button-nav-not"
+            }
+            onClick={() => handleNavItemClick("home")}
+          >
+            <div
+              className={
+                activeNavItem === "home"
+                  ? "button-nav-not-home"
+                  : "button-nav-home"
+              }
+            />
+            Accueil
+          </button>
+        </div>
+      </NavLink>
+
+      <NavLink
+        to="/challenges"
+        activeclassname="activelink"
+        onClick={() => handleTabClick("defis")}
       >
-        <button
-          type="button"
-          className={
-            activeNavItem === "target"
-              ? "button-nav-not-target"
-              : "button-nav-target"
-          }
-          label="icon-home"
-        />
-        <p>Défis</p>
-      </button>
-      <button
-        type="button"
-        className={
-          activeNavItem === "classement" ? "button-nav" : "button-nav-not"
-        }
-        onClick={() => handleNavItemClick("classement")}
+        <div className="button-link">
+          <button
+            type="button"
+            className={
+              activeNavItem === "target" ? "button-nav" : "button-nav-not"
+            }
+            onClick={() => handleNavItemClick("target")}
+          >
+            <div
+              className={
+                activeNavItem === "target"
+                  ? "button-nav-not-target"
+                  : "button-nav-target"
+              }
+            />
+
+            <p>Défis</p>
+          </button>
+        </div>
+      </NavLink>
+
+      <NavLink
+        to="/classement"
+        activeclassname="activelink"
+        onClick={() => handleTabClick("classement")}
       >
-        <button
-          type="button"
-          className={
-            activeNavItem === "classement"
-              ? "button-nav-not-classement"
-              : "button-nav-classement"
-          }
-          label="icon-home"
-        />
-        <p>Classement</p>
-      </button>
-      <button
-        type="button"
-        className={
-          activeNavItem === "button-tips" ? "button-nav" : "button-nav-not"
-        }
-        onClick={() => handleNavItemClick("button-tips")}
+        <div className="button-link">
+          <button
+            type="button"
+            className={
+              activeNavItem === "classement" ? "button-nav" : "button-nav-not"
+            }
+            onClick={() => handleNavItemClick("classement")}
+          >
+            <div
+              className={
+                activeNavItem === "classement"
+                  ? "button-nav-not-classement"
+                  : "button-nav-classement"
+              }
+            />
+
+            <p>Classement</p>
+          </button>
+        </div>
+      </NavLink>
+
+      <NavLink
+        to="/tips"
+        activeclassname="activelink"
+        onClick={() => handleTabClick("defis")}
       >
-        <button
-          type="button"
-          className={
-            activeNavItem === "button-tips"
-              ? "button-nav-not-tips"
-              : "button-nav-tips"
-          }
-          label="icon-home"
-        />
-        <p>Astuces</p>
-      </button>
+        <div className="button-link">
+          <button
+            type="button"
+            className={
+              activeNavItem === "button-tips" ? "button-nav" : "button-nav-not"
+            }
+            onClick={() => handleNavItemClick("button-tips")}
+          >
+            <div
+              className={
+                activeNavItem === "button-tips"
+                  ? "button-nav-not-tips"
+                  : "button-nav-tips"
+              }
+            />
+            <p>Défis</p>
+          </button>
+        </div>
+      </NavLink>
     </div>
   );
 }
