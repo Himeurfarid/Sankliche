f648e219ec26b1521fdb137518950ee2f4ba5926
AlexisCode22 <durinalexis@gmail.com>
2023-03-02 16:10:16 +0100

    second version profil page

--- frontend/src/pages/Profil.jsx
+++ frontend/src/pages/Profil.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/Profil.css";
@@ -8,6 +8,7 @@ const DEFAULT_PICTURE = PictureDefault;
 
 function Profil() {
   const [picture, setPicture] = useState(DEFAULT_PICTURE);
+  const fileInputRef = useRef();
 
   function handlePictureChange(event) {
     const selectedFile = event.target.files[0];
@@ -24,17 +25,35 @@ function Profil() {
     }
   }
 
+  function handlePictureClick() {
+    fileInputRef.current.click();
+  }
+
+  function handlePictureKeyDown(event) {
+    if (event.key === "Enter") {
+      handlePictureClick();
+    }
+  }
+
   return (
     <>
       <Header />
       <div className="topProfil">
-        {picture ? (
-          <img src={picture} alt="" className="picture" />
-        ) : (
-          <div>
-            <input type="file" onChange={handlePictureChange} />
-          </div>
-        )}
+        <div
+          className="profil-picture"
+          onClick={handlePictureClick}
+          onKeyDown={handlePictureKeyDown}
+          tabIndex="0"
+          role="button"
+        >
+          {picture ? (
+            <img src={picture} alt="" className="picture" />
+          ) : (
+            <div className="picture-placeholder">Sélectionner une photo</div>
+          )}
+        </div>
+        <input type="file" onChange={handlePictureChange} ref={fileInputRef} />
+
         <div className="names">
           <h1 className="styleName">Alexis</h1>
           <h2 className="styleName">Durin</h2>
